f606a6e5f194e779b0213f0e0bd1863af304ddda
Leaf08 <leaf08@yandex.ru>
2021-07-09 11:42:55 +0300

    init: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) n *= -1;
+    if (n % 10 === 0) n /= 10;
+    return Number(n.toString().split('').reverse().join(''));
 }
